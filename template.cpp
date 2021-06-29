e59a342bce95387927e7822717e8aee0b31deb45
vaibhavramchandani20 <85990756+vaibhavramchandani20@users.noreply.github.com>
2021-06-29 10:20:23 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define deb(x) cout << #x << "=" << x << endl
+
+
+//This is a template used for CP
+
+int main() {
+	// your code goes here
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	    
+	}
+	return 0;
+}
